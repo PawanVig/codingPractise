b0d9d7e153d6088a3fcceea9b7a947d4ae1032f3
pawanvig <pawan1119vig@gmail.com>
2020-05-31 21:56:48 -0400

    2 sum in python added

---
+++ leetcode2SumInPython.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+            delta = target - nums[i]
+            if delta in dic.keys():
+                return {dic[delta], i}
+            dic[nums[i]] = i
+        return []
